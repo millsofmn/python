529f18c8453384f16a4f7d79d97ebedea23996c3
Miller <Miller.Lyle@mayo.edu>
2016-07-18 13:52:38 -0500

    Simple examples

---
+++ play-ground/numbers.py
@@ -0,0 +1,11 @@
+import math
+
+num_1 = '200'
+num_2 = 400
+
+# Get powers of
+num_1_answer = math.pow(int(num_1), 2)
+print 'Square of string number', num_1, 'is', num_1_answer
+
+num_2_answer = math.pow(num_2, 2)
+print 'Square of integer', num_2, 'is', num_1_answer
